575cf60957a7c96319a02a535bb53987c8fd637a
Akshay Patil <akshay.patil@dspl.com>
2025-12-17 09:39:22 +0530

    empty wishlist

--- components/Shopping/WishList/WishList.controller.js
+++ components/Shopping/WishList/WishList.controller.js
@@ -907,7 +907,7 @@ module.exports = {
 
             let data = await module.exports.getWishListData(matchCondition)
             if (data && data[0]?.Products?.length == 0) {
-                return res.status(200).json({ message: 'Cart is empty', success: false,data:data })
+                return res.status(200).json({ message: 'Cart is empty', success: true,data:data })
             }
             if (data?.length) {
                 data = data.map(cart => {
