5f5a0e8d5f1486053da9dd85ff9d7e7407c50aa3
Akshay Patil <akshay.patil@dspl.com>
2026-04-10 12:58:15 +0530

    changesz

--- Routes/DoorStepService.routes.js
+++ Routes/DoorStepService.routes.js
@@ -1,12 +1,14 @@
 const ServiceCategoryRoutes = require('../components/DoorStepService/ServiceCategory/ServiceCategory.routes')
 const ServiceBannerRoutes = require('../components/DoorStepService/ServiceBanners/ServiceBanners.routes')
 const ServiceProductsRoutes = require('../components/DoorStepService/ServiceProducts/ServiceProducts.routes')
+const ServiceProvidersRoutes = require('../components/DoorStepService/ServiceProducts/ServiceProducts.routes')
 
 
 const apiString = `/api/${process.env.API_VERSION}`;
 const ServiceCategoryString = apiString + '/serviceCategories'
 const ServiceBannersString = apiString + '/serviceBanners'
 const ServiceProductsString = apiString + '/serviceProducts'
+const ServiceProvidersString = apiString + '/serviceProviders'
 
 
 
@@ -14,4 +16,5 @@ exports.default = (app) => {
     app.use(ServiceCategoryString, ServiceCategoryRoutes)
     app.use(ServiceBannersString, ServiceBannerRoutes)
     app.use(ServiceProductsString, ServiceProductsRoutes)
+    app.use(ServiceProvidersString, ServiceProvidersRoutes)
 }
